b0c1d4977f91b6019215f5b92ca4e7ebb0f11f16
Rohmad Eko Wahyudi <eko@impstudio.co.id>
2024-10-02 21:28:00 +0700

    sidebar

--- src/components/layout/sidebar/Main.tsx
+++ src/components/layout/sidebar/Main.tsx
@@ -1,3 +1,4 @@
+import { GoPlus } from "react-icons/go";
 import logo from "../../../assets/logo.png";
 
 export default function SidebarMain() {
@@ -7,11 +8,14 @@ export default function SidebarMain() {
       <hr />
       <div className="mt-[32px] text-[16px] text-[#757575]">Projects</div>
       <div className="mt-[16px]">
-        <button className="btn btn-primary w-full">Garfield</button>
+        <button className="btn btn-primary w-full">
+          <div className="text-left w-full">Garfield</div>
+        </button>
       </div>
       <div className="mt-[32px]">
         <button className="btn btn-primary btn-outline w-full">
-          Create New Project
+          <GoPlus className="w-[20px] h-[20px]" />
+          <div className="font-normal">Create New Project</div>
         </button>
       </div>
     </div>
