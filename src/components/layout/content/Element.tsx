d6384dcfc4b62605ac86e967ff53168d6c023c39
Rohmad Eko Wahyudi <eko@impstudio.co.id>
2024-10-02 22:26:45 +0700

    side elemetn

--- src/components/layout/content/Element.tsx
+++ src/components/layout/content/Element.tsx
@@ -2,7 +2,7 @@ import { GoChevronLeft } from "react-icons/go";
 
 export default function ContentElement() {
   return (
-    <div className="w-[352px] h-full rounded-[8px] border border-[#ECECEC]">
+    <div className="w-[352px] h-full rounded-[8px] border border-[#ECECEC] divide-y">
       <div className="p-[16px] h-[68px]">
         <div className="flex items-center gap-[16px]">
           <a
@@ -15,7 +15,49 @@ export default function ContentElement() {
           <div className="text-[14px] text-[#7F74FF]">75%</div>
         </div>
       </div>
-      <hr />
+      <div className="collapse collapse-arrow rounded-none">
+        <input type="checkbox" defaultChecked />
+        <div className="collapse-title flex items-center">
+          <div className="text-[16px] font-medium mr-3">Untested Elements</div>
+          <div className="px-2 py-1 bg-blue-50 text-blue-400 rounded-full text-xs">
+            2
+          </div>
+        </div>
+        <div className="collapse-content space-y-1">
+          <div className="rounded-[8px] border-l-8 border border-[#FF7070] px-[24px] py-[12px]">
+            form/input/email
+          </div>
+          <div className="rounded-[8px] border-l-8 border border-[#FF7070] px-[24px] py-[12px]">
+            form/input/reasonforreachout
+          </div>
+        </div>
+      </div>
+      <div className="collapse collapse-arrow rounded-none">
+        <input type="checkbox" />
+        <div className="collapse-title flex items-center">
+          <div className="text-[16px] font-medium mr-3">Tested Elements</div>
+          <div className="px-2 py-1 bg-blue-50 text-blue-400 rounded-full text-xs">
+            5
+          </div>
+        </div>
+        <div className="collapse-content space-y-1">
+          <div className="rounded-[8px] border-l-8 border border-[#6DFF8D] px-[24px] py-[12px]">
+            form/input/firstname
+          </div>
+          <div className="rounded-[8px] border-l-8 border border-[#6DFF8D] px-[24px] py-[12px]">
+            form/input/lastname
+          </div>
+          <div className="rounded-[8px] border-l-8 border border-[#6DFF8D] px-[24px] py-[12px]">
+            form/input/companyname
+          </div>
+          <div className="rounded-[8px] border-l-8 border border-[#6DFF8D] px-[24px] py-[12px]">
+            form/input/jobtitle
+          </div>
+          <div className="rounded-[8px] border-l-8 border border-[#6DFF8D] px-[24px] py-[12px]">
+            form/input/submit
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
