89eb11b1c3d18889c6fb2d2f9af8d7a5fb1ccbc9
Rohmad Eko Wahyudi <eko@impstudio.co.id>
2024-10-03 08:10:14 +0700

    order table

--- src/components/coverage_over_time/Table.tsx
+++ src/components/coverage_over_time/Table.tsx
@@ -1,3 +1,4 @@
+import { IoIosArrowRoundDown, IoIosArrowRoundUp } from "react-icons/io";
 import { dataCoverageOverTime } from "../../types/run_testing";
 
 export default function CoverageOverTimeTable() {
@@ -5,23 +6,53 @@ export default function CoverageOverTimeTable() {
     <table className="table table-zebra rounded-lg">
       <thead>
         <tr className="bg-[#F1F5FF] text-[#111111] text-[12px]">
-          <th className="font-normal">Name</th>
+          <th>
+            <div className="flex items-center">
+              <div className="font-normal mr-2">Name</div>
+              <IoIosArrowRoundDown className="text-[#8FA1D1] w-[16px] h-[16px]" />
+              <IoIosArrowRoundUp className="text-[#8FA1D1] -ml-2 w-[16px] h-[16px]" />
+            </div>
+          </th>
           <th className="font-normal">Untested</th>
           <th className="font-normal">Tested</th>
-          <th className="font-normal">Coverage Percentage</th>
-          <th className="font-normal">Timestamp</th>
+          <th>
+            <div className="flex items-center">
+              <div className="font-normal mr-2">Coverage Percentage</div>
+              <IoIosArrowRoundDown className="text-[#8FA1D1] w-[16px] h-[16px]" />
+              <IoIosArrowRoundUp className="text-[#8FA1D1] -ml-2 w-[16px] h-[16px]" />
+            </div>
+          </th>
+          <th>
+            <div className="flex items-center">
+              <div className="font-normal mr-2">Timestamp</div>
+              <IoIosArrowRoundDown className="text-[#8FA1D1] w-[16px] h-[16px]" />
+              <IoIosArrowRoundUp className="text-[#8FA1D1] -ml-2 w-[16px] h-[16px]" />
+            </div>
+          </th>
         </tr>
       </thead>
       <tbody className="text-base">
-        {dataCoverageOverTime.map((data, index) => (
-          <tr key={index}>
-            <td>{data.name}</td>
-            <td>{data.untested}</td>
-            <td>{data.tested}</td>
-            <td>{data.coveragePercentage}%</td>
-            <td>{data.timestamp}</td>
-          </tr>
-        ))}
+        {dataCoverageOverTime
+          .slice()
+          .reverse()
+          .map((data, index) => (
+            <tr key={index}>
+              <td>{data.name}</td>
+              <td>{data.untested}</td>
+              <td>{data.tested}</td>
+              <td>
+                <div className="flex items-center gap-2">
+                  <progress
+                    className="progress progress-accent w-[80px]"
+                    value={data.coveragePercentage}
+                    max="100"
+                  ></progress>
+                  <div>{data.coveragePercentage}%</div>
+                </div>
+              </td>
+              <td>{data.timestamp}</td>
+            </tr>
+          ))}
       </tbody>
     </table>
   );
