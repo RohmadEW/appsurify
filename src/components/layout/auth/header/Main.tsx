307e65d6a900384fb6efbbc3fa3a7bdab8b9671b
Rohmad Eko Wahyudi <eko@impstudio.co.id>
2024-10-25 21:20:45 +0700

    show name and email on header

--- src/components/layout/auth/header/Main.tsx
+++ src/components/layout/auth/header/Main.tsx
@@ -1,17 +1,21 @@
 import { BsChatDots } from "react-icons/bs";
 import { PiSignIn } from "react-icons/pi";
-import avatar from "../../../../assets/avatar.png";
+import { useAppSelector } from "../../../../hooks/useStore";
 
 export default function HeaderMain() {
+  const { user } = useAppSelector((state) => state.auth);
+
   return (
     <div className="w-full h-[58px] flex items-center justify-end gap-3 px-[24px]">
       <button className="btn btn-sm btn-primary btn-outline font-normal px-8 mr-8">
         <BsChatDots className="w-[15px] h-[15px]" />
         Provide Feedback
       </button>
-      <div className="text-base text-[#393939]">Brooklyn Simmons</div>
+      <div className="text-base text-[#393939]">
+        {user?.first_name} {user?.last_name} ({user?.email})
+      </div>
       <img
-        src={avatar}
+        src={user?.avatar_url}
         alt="avatar"
         className="w-[32px] h-[32px] rounded-full"
       />
